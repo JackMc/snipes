a45f8b8d8614d9ac98721deea31085b314185104
Jack McCracken <jackamc19@gmail.com>
2011-02-04 00:51:59 +0000

    Very beginnings of the actual bot.

---
+++ testing/bot/src/org/ossnipes/snipes/bot/Main.java
@@ -0,0 +1,12 @@
+package org.ossnipes.snipes.bot;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jack
+ * Date: 03/02/11
+ * Time: 7:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+}
